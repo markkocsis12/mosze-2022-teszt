7055c63c4b4aac0561f4a12f6c5e92314ecde6df
markkocsis12 <71631248+markkocsis12@users.noreply.github.com>
2022-09-19 14:02:59 +0200

    hibak dokumentalasa

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
+    int *b = new int[NELEMENTS]; //int* b, N_ELEMENTS
+    std::cout << '1-100 ertekek duplazasa' //dupla "" és ; a sor végére és egy sortörés
     for (int i = 0;)
     {
-        b[i] = i * 2;
+        b[i] = i * 2; // b[i-1]
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // felesleges ciklus, konkrét érték kiiratasa sortoressel es ; a vegere
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i] // pontosvesszo
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
